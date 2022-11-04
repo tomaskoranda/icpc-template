5c83fe68a62e5e28bb5795d7b58384562bc0d39f
Tomas Koranda <tom.koranda@gmail.com>
2022-11-04 22:39:10 +0100

    Add cpp template

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define FOR(i, L, R) for (int i = L; i < R; i++)
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define int long long int
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    /* ---------------------------------------------------------------------- */
+
+    int test_cases = 1;
+    while (test_cases--)
+    {
+    }
+
+    return 0;
+}
